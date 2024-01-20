93f35d2ded674bd0b73cfdaa2d986d88c2ff9639
jeonmmingu <jeonalsrn@gmail.com>
2024-01-21 02:31:45 +0900

    add docRefUser data in userData List + implement Delete User Data func

--- lib/get_data/get_screen.dart
+++ lib/get_data/get_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_getset_userdata/get_data/get_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class GetScreen extends StatelessWidget {
   const GetScreen({Key? key}) : super(key: key);
@@ -39,6 +40,8 @@ class GetScreen extends StatelessWidget {
                           "name": doc["name"] ?? "Error name",
                           "age": doc["age"] ?? "Error age",
                           "gender": doc["gender"] ?? "Error gender",
+                          // userDataList에 유저 정보를 담고 있는 firebase doc 주소를 저장
+                          "docRefUser": doc.reference,
                         })
                     .toList();
 
@@ -115,11 +118,115 @@ class PersonContainer extends StatelessWidget {
 class UserDetailsScreen extends StatelessWidget {
   final Map<String, dynamic> userData;
 
-  const UserDetailsScreen({required this.userData, Key? key}) : super(key: key);
+  const UserDetailsScreen({
+    required this.userData,
+    Key? key,
+  }) : super(key: key);
 
   void modifyButtonOnTap() {}
 
-  void updateButtonOnTap() {}
+  void deleteButtonTapped({required BuildContext context}) {
+    // TODO: 정말로 삭제할 것이냐는 알림 팝업 출력
+    createDeleteUserDialog(
+      context: context,
+      title: "유저 정보를 삭제하시겠습니까?",
+      description: "삭제된 정보는 복원할 수 없습니다.",
+    );
+  }
+
+  void createDeleteUserDialog({
+    required BuildContext context,
+    required String title,
+    required String description,
+  }) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          //Dialog Main Title
+          title: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          // Content Description
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                description,
+              ),
+            ],
+          ),
+          // ConfirmButton
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  child: Text(
+                    "취소",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red[300],
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                TextButton(
+                  child: Text(
+                    "확인",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[300],
+                    ),
+                  ),
+                  onPressed: () {
+                    print("확인버튼이 눌렸습니다....");
+                    deleteUserData(
+                      context: context,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  /// 유저 정보를 DB에서 삭제하는 함수
+  void deleteUserData({
+    required BuildContext context,
+  }) {
+    userData["docRefUser"].delete().then((value) {
+      print("유저 정보 삭제 완료 !");
+      Navigator.popUntil(context, ModalRoute.withName("/"));
+    }).catchError((err) {
+      print("유저 정보 삭제 실패....");
+      Navigator.pop(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -162,7 +269,9 @@ class UserDetailsScreen extends StatelessWidget {
                 width: 20,
               ),
               IconButton(
-                onPressed: updateButtonOnTap,
+                onPressed: () {
+                  return deleteButtonTapped(context: context);
+                },
                 icon: const Icon(
                   Icons.delete_forever,
                   size: 25,
