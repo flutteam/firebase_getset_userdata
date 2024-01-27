4da45cdc883c62645e5df730a11b4f7f84568ca7
서은진 <eeeunjin@seoeunjin-ui-noteubug.local>
2024-01-28 00:13:16 +0900

    update screen's UI need to update

--- lib/update_data/update_screen.dart
+++ lib/update_data/update_screen.dart
@@ -15,8 +15,6 @@ class _UpdateScreenState extends State<UpdateScreen> {
   int newAge = 0;
   String newGender = "";
 
-  String dropdownValue = '';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,39 +70,31 @@ class _UpdateScreenState extends State<UpdateScreen> {
                   });
                 },
               ),
-              // Use DropdownButton for gender selection
-              // Stack(
-              //   children: [
-              TextFormField(
-                initialValue: widget.userData["gender"] ?? "",
-                decoration: const InputDecoration(labelText: "Gender"),
-                onChanged: (value) {
-                  setState(() {
-                    newGender = value;
-                  });
+              GestureDetector(
+                onTap: () async {
+                  String? selectedGender = await showGenderSelectionDialog();
+                  if (selectedGender != null) {
+                    setState(() {
+                      newGender = selectedGender;
+                    });
+                  }
                 },
+                child: TextFormField(
+                  initialValue: newGender,
+                  decoration: const InputDecoration(
+                    labelText: "Gender",
+                  ),
+                  readOnly: true,
+                  onTap: () async {
+                    String? selectedGender = await showGenderSelectionDialog();
+                    if (selectedGender != null) {
+                      setState(() {
+                        newGender = selectedGender;
+                      });
+                    }
+                  },
+                ),
               ),
-              // DropdownButton<String>(
-              //   value: dropdownValue, // 현재 선택된 값
-              //   hint: const Text("Select Gender"),
-              //   icon: const Icon(Icons.arrow_downward),
-              //   iconSize: 24,
-              //   elevation: 16, // 드롭다운이 떠있을 때의 고도
-              //   style: const TextStyle(color: Colors.deepPurple),
-              //   onChanged: (String? newValue) {
-              //     setState(() {
-              //       dropdownValue = newValue!; // 새로운 값이 선택될 때마다 상태 업데이트
-              //       newGender = newValue;
-              //     });
-              //   },
-              //   items: <String>['', 'man', 'woman'] // 드롭다운에 표시될 항목들
-              //       .map<DropdownMenuItem<String>>((String value) {
-              //     return DropdownMenuItem<String>(
-              //       value: value,
-              //       child: Text(value),
-              //     );
-              //   }).toList(),
-              // ),
             ],
           ),
         ),
@@ -159,48 +149,75 @@ class _UpdateScreenState extends State<UpdateScreen> {
     required BuildContext context,
   }) async {
     // 사용자의 문서에 대한 참조 얻기
-    DocumentReference docRef = widget.userData["docRef"];
+    DocumentReference<Map<String, dynamic>>? docRef = widget.userData["docRef"];
 
-    // 사용자 문서의 현재 스냅샷 얻기
-    DocumentSnapshot userSnapshot = await docRef.get();
+    if (docRef != null) {
+      // 사용자 문서의 현재 스냅샷 얻기
+      DocumentSnapshot<Map<String, dynamic>> userSnapshot = await docRef.get();
 
-    if (userSnapshot.exists) {
-      print("사용자 정보 업데이트 중...");
+      if (userSnapshot.exists) {
+        print("사용자 정보 업데이트 중...");
 
-      Map<String, dynamic> updatedData = {};
+        Map<String, dynamic> updatedData = {};
 
-      // 새 값이 현재 값과 다른지 확인
-      if (newName.isNotEmpty && newName != widget.userData["name"]) {
-        updatedData["name"] = newName;
-      }
-      if (newAge != 0 && newAge != widget.userData["age"]) {
-        updatedData["age"] = newAge;
-      }
-      if (newGender.isNotEmpty && newGender != widget.userData["gender"]) {
-        updatedData["gender"] = newGender;
-      }
+        // 새 값이 현재 값과 다른지 확인
+        if (newName.isNotEmpty && newName != widget.userData["name"]) {
+          updatedData["name"] = newName;
+        }
+        if (newAge != 0 && newAge != widget.userData["age"]) {
+          updatedData["age"] = newAge;
+        }
+        if (newGender.isNotEmpty && newGender != widget.userData["gender"]) {
+          updatedData["gender"] = newGender;
+        }
 
-      // 변경된 값이 있는 경우에만 업데이트 수행
-      if (updatedData.isNotEmpty) {
-        await docRef.update(updatedData).then((_) {
-          // 업데이트가 성공하면 화면을 닫고 업데이트된 데이터를 제공
-          Navigator.pop(context, {
-            "name": updatedData["name"] ?? widget.userData["name"],
-            "age": updatedData["age"] ?? widget.userData["age"],
-            "gender": updatedData["gender"] ?? widget.userData["gender"],
+        // 변경된 값이 있는 경우에만 업데이트 수행
+        if (updatedData.isNotEmpty) {
+          await docRef.update(updatedData).then((_) {
+            // 업데이트가 성공하면 화면을 닫고 업데이트된 데이터를 제공
+            Navigator.pop(context, {
+              "name": updatedData["name"] ?? widget.userData["name"],
+              "age": updatedData["age"] ?? widget.userData["age"],
+              "gender": updatedData["gender"] ?? widget.userData["gender"],
+            });
+            print("사용자 정보가 업데이트되었습니다");
+          }).catchError((error) {
+            // 업데이트 중 오류 처리
+            print("사용자 정보 업데이트 중 오류 발생: $error");
           });
-          print("사용자 정보가 업데이트되었습니다");
-        }).catchError((error) {
-          // 업데이트 중 오류 처리
-          print("사용자 정보 업데이트 중 오류 발생: $error");
-        });
+        } else {
+          print("변경된 값이 없습니다.");
+          // 변경된 값이 없으면 이전 화면으로 바로 이동
+          Navigator.pop(context);
+        }
       } else {
-        print("변경된 값이 없습니다.");
-        // 변경된 값이 없으면 이전 화면으로 바로 이동
-        Navigator.pop(context);
+        print("문서를 찾을 수 없습니다. 사용자 정보 업데이트 불가능");
       }
-    } else {
-      print("문서를 찾을 수 없습니다. 사용자 정보 업데이트 불가능");
     }
+  }
+
+  Future<String?> showGenderSelectionDialog() async {
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Select Gender'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, 'man');
+              },
+              child: const Text('Man'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, 'woman');
+              },
+              child: const Text('Woman'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
